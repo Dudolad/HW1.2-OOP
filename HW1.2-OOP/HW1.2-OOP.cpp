74f7f34056243f88f8b00109e7ba50939e752fea
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-14 20:20:48 +0300

    Added main

--- HW1.2-OOP/HW1.2-OOP.cpp
+++ HW1.2-OOP/HW1.2-OOP.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include "Plane.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    Airplane plane1;
+    plane1.showInfo();
+
+    Airplane plane2("PS123", "Kyiv", "London", "Boeing 737", 180, 100);
+    plane2.showInfo();
+
+    plane1.setFlightNumber("AA987");
+    plane1.setDeparture("New York");
+    plane1.setDestination("Paris");
+    plane1.setBrand("Airbus A320");
+    plane1.setTotalSeats(150);
+    plane1.setBookedSeats(50);
+    plane1.showInfo();
+
+    plane1.sellTicket(20);    
+    plane1.sellTicket(100);   
+    plane1.returnTicket(30);  
+    plane1.returnTicket(100); 
+
+    plane1.showInfo();
+
+    return 0;
 }
 
